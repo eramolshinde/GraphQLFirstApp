51767ef3f33a9d16e540b44639466d7c69222294
Amol Shinde <amol.shinde@eviden.com>
2024-02-14 17:59:35 +0530

    Added query and Configure Startup with HotChocolate middleware

--- Program.cs
+++ Program.cs
@@ -1,25 +1,16 @@
+using GraphQLFirstApp.Types;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddGraphQLServer().AddQueryType<Query>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseRouting().UseEndpoints(endpoints => endpoints.MapGraphQL());
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+app.MapGet("/", () => "Hello World");
 app.Run();
---
+++ Types/Query.cs
@@ -0,0 +1,18 @@
+﻿namespace GraphQLFirstApp.Types
+{
+    public class Query
+    {
+        readonly List<Book> _books = new()
+        {
+            new Book("I love GrapgQL", new Author("Jhon")),
+            new Book("GrapgQL is feature", new Author("Amol")),
+            new Book("I love SOAP + Azure", new Author("Ram"))
+        };
+
+        public List<Book> GetBooks() => _books;
+    }
+    public record Book(string Title, Author Author);
+
+    public record Author(string Name);
+
+}
